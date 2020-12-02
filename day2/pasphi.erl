a5509435731d12dedf0e0672efdf738008dd7605
FKD13 <francisklinck@gmail.com>
2020-12-02 17:09:41 +0100

    update day2

--- day2/pasphi.erl
+++ day2/pasphi.erl
@@ -16,8 +16,10 @@ go(FileName) ->
   {_, IoDev} = file:open(FileName, read),
   Lines = [{[list_to_integer(X) || X <- string:tokens(Bounds, "-")], string:strip(Letter, both, $:), Password} || [Bounds, Letter, Password] <- [string:split(Line, " ", all) || Line <- read_lines(IoDev)]],
   io:format("~p~n", [Lines]),
-  Correct = lists:filter(fun({[Lower, Upper], [Letter], Password}) -> is_valid2(Lower, Upper, Letter, Password) end, Lines),
-  io:format("Correct ~p~n", [length(Correct)]).
+  Correct = lists:filter(fun({[Lower, Upper], [Letter], Password}) -> is_valid(Lower, Upper, Letter, Password) end, Lines),
+  io:format("Correct1 ~p~n", [length(Correct)]),
+  Correct2 = lists:filter(fun({[Lower, Upper], [Letter], Password}) -> is_valid2(Lower, Upper, Letter, Password) end, Lines),
+  io:format("Correct2 ~p~n", [length(Correct2)]).
   
 is_valid(Lower, Upper, Letter, Password) ->
   Count = length([C || C <- Password, C =:= Letter]),
