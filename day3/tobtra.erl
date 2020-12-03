5f36a3f98194d6dcd1dfc4c34a8a7135bd1bcca6
Francis <francisklinck@gmail.com>
2020-12-03 14:02:45 +0100

    Fix some words

--- day3/tobtra.erl
+++ day3/tobtra.erl
@@ -15,28 +15,25 @@ read_lines(IoDev) ->
 go(FileName) ->
   {_, IoDev} = file:open(FileName, read),
   Forest = read_lines(IoDev),
-  io:format("Answer1: ~p~n", [init_desend(3, Forest)]),
+  io:format("Answer1: ~p~n", [init_descend(3, Forest)]),
   io:format("Answer2: ~p~n", 
     [
-      init_desend(1, Forest) *
-      init_desend(3, Forest) *
-      init_desend(5, Forest) *
-      init_desend(7, Forest) * 
-      init_desend(1, cutdown_oneven_trees(0, Forest))
+      lists:foldl(fun(V, Acc) -> init_descend(V, Forest) * Acc end, 1, [1, 3, 5, 7]) *
+      init_descend(1, cutdown_odd_trees(0, Forest))
     ]).
 
-cutdown_oneven_trees(_, []) -> [];
-cutdown_oneven_trees(N, [T|Forest]) when (N band 1) == 0 -> [T|cutdown_oneven_trees(N+1, Forest)];
-cutdown_oneven_trees(N, [_|Forest]) -> cutdown_oneven_trees(N+1, Forest).
+cutdown_odd_trees(_, []) -> [];
+cutdown_odd_trees(N, [T|Forest]) when (N band 1) == 0 -> [T|cutdown_odd_trees(N+1, Forest)];
+cutdown_odd_trees(N, [_|Forest]) -> cutdown_odd_trees(N+1, Forest).
 
-init_desend(Rate, Forest) -> desend(1+Rate, length(hd(Forest))+1, Rate, Forest).
+init_descend(Rate, Forest) -> descend(1+Rate, length(hd(Forest))+1, Rate, Forest).
 
-desend(_, _, _, [_ |[]]) -> 0;
-desend(X, Width, Rate, F) when X >= Width -> desend(mod(X, Width)+1, Width, Rate, F);
-desend(X, Width, Rate, [_, Next | Forest]) ->
+descend(_, _, _, [_ |[]]) -> 0;
+descend(X, Width, R, F) when X >= Width -> descend(mod(X, Width)+1, Width, R, F);
+descend(X, Width, Rate, [_, Next | Forest]) ->
   case lists:nth(X, Next) of
-    $# -> 1 + desend(X + Rate, Width, Rate, [Next | Forest]);
-    $. -> desend(X + Rate, Width, Rate, [Next | Forest])
+    $# -> 1 + descend(X + Rate, Width, Rate, [Next | Forest]);
+    $. -> descend(X + Rate, Width, Rate, [Next | Forest])
   end.
 
 mod(X, Y) -> (X rem Y + Y) rem Y.
