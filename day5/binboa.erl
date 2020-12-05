ce1cd34328100c27ee4c801310726779a98814cb
FKD13 <francisklinck@gmail.com>
2020-12-05 14:09:33 +0100

    update solution

--- day5/binboa.erl
+++ day5/binboa.erl
@@ -19,12 +19,12 @@ go(FileName) ->
   solve_second(Lines).
 
 solve_first(Lines) ->
-  Max = lists:max(lists:map(fun(X) -> calculate_seat_value(X) end, Lines)),
+  Max = lists:max(calculate_seat_values(Lines)),
   io:format("First: ~p~n", [Max]),
   ok.
 
 solve_second(Lines) ->
-  Scores = lists:map(fun(X) -> calculate_seat_value(X) end, Lines),
+  Scores = calculate_seat_values(Lines),
   io:format("Second: ~p~n", [find_seat(lists:sort(Scores))]),
   ok.
 
@@ -32,16 +32,10 @@ find_seat([_])                        -> no_seat;
 find_seat([F, S | _]) when F + 2 == S -> F + 1;
 find_seat([_ | Rest])                 -> find_seat(Rest).
 
-calculate_seat_value(Line) ->
-  {F, L} = parse_input(Line, []),
-  score(F, "B") * 8 + score(L, "R").
+calculate_seat_values(Lines) -> lists:map(fun(X) -> score(X, "BR") end, Lines).
 
-parse_input([$F | Letters ], First) -> parse_input(Letters, lists:append(First, "F"));
-parse_input([$B | Letters ], First) -> parse_input(Letters, lists:append(First, "B"));
-parse_input(Last, First) -> {First, Last}.
-
-score([L], Positive) -> return_when_in_list(1, L, Positive);
-score([L| Letters], Positive) -> return_when_in_list(math:pow(2, length(Letters)), L, Positive) + score(Letters, Positive).
+score([L], Keep) -> return_when_in_list(1, L, Keep);
+score([L| Letters], Keep) -> return_when_in_list(math:pow(2, length(Letters)), L, Keep) + score(Letters, Keep).
 
 return_when_in_list(Return, Letter, List) ->
   case lists:any(fun(X)-> Letter =:= X end, List) of
